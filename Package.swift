17cd36ad1948066cfb2a8071898a286c474f4946
Marius Ilie <marius@iliemarius.ro>
2019-10-26 21:01:29 +0300

    Package.swift without RxCocoa as dep.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxReachability",
-            dependencies: ["RxSwift", "RxCocoa", "Reachability"]),
+            dependencies: ["RxSwift", "Reachability"]),
         .testTarget(
             name: "RxReachabilityTests",
             dependencies: ["RxReachability"]),
