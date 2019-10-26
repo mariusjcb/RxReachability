770692610b8105b53c26e82b5849fca18c3c8410
Marius Ilie <marius@iliemarius.ro>
2019-10-26 19:34:10 +0300

    Missing ReachabilitySwift package fix

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxReachability",
-            dependencies: ["RxSwift", "RxCocoa", "ReachabilitySwift"]),
+            dependencies: ["RxSwift", "RxCocoa", "Reachability"]),
         .testTarget(
             name: "RxReachabilityTests",
             dependencies: ["RxReachability"]),
