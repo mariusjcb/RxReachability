595b25adfa0e3e61ac4a5b3801bc6ae0e1285df6
Marius Ilie <mariusjcb@users.noreply.github.com>
2019-10-26 18:48:37 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "RxReachability",
-    products: [ ],
+    products: [
+        .library(name: "RxReachability", targets: ["RxReachability"])
+    ],
     dependencies: [
         .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
@@ -12,10 +14,12 @@ let package = Package(
     targets: [
         .target(
             name: "RxReachability",
-            dependencies: ["RxSwift", "RxCocoa", "ReachabilitySwift"]),
+            dependencies: ["RxSwift", "RxCocoa", "ReachabilitySwift"],
+            path: "RxReachability"),
         .testTarget(
             name: "RxReachability-Tests",
-            dependencies: ["RxReachability"]),
+            dependencies: ["RxReachability"],
+            path: "RxReachability-Tests"),
     ],
     swiftLanguageVersions: [.v5]
 )
